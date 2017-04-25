6d83883ff16f4e6b54510aac8afd116c805d3ab4
Ilya Minkin <ilya.minkinen@gmail.com>
2017-04-25 13:43:50 -0400

    Got rid of "test"

--- msvs/sibelia-pp/test/test2/strip_fa.py
+++
@@ -1,9 +0,0 @@
-import sys
-import collections
-from Bio import SeqIO
-
-for record in SeqIO.parse(sys.argv[1], "fasta"):
-	print '>' + record.id
-	print ''.join((x for x in record.seq if x != '-'))
-
-

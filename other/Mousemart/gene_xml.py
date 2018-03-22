d75ffde069594ccf81dd9e2a278e1de366248e84
ilyaminkin <ivminkin@gmail.com>
2018-03-22 15:18:20 -0400

    Updated mousemart scripts

--- other/Mousemart/gene_xml.py
+++ other/Mousemart/gene_xml.py
@@ -11,7 +11,41 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
 	</Dataset>
 </Query>'''
 
+hmlg_template = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+        <Dataset name = "%s_gene_ensembl" interface = "default" >
+		<Attribute name = "ensembl_gene_id" />
+		<Attribute name = "%s_homolog_ensembl_gene" />
+	</Dataset>
+</Query>'''
+
+paralog_template = '''<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE Query>
+<Query  virtualSchemaName = "default" formatter = "TSV" header = "0" uniqueRows = "0" count = "" datasetConfigVersion = "0.6" >
+        <Dataset name = "%s_gene_ensembl" interface = "default" >
+                <Attribute name = "ensembl_gene_id" />
+                <Attribute name = "%s_paralog_ensembl_gene" />
+        </Dataset>
+</Query>'''
+
+
+strain = []
+hmlg = []
+
 for line in open("mice.txt"):
-	strain = line.strip().split()[1].split('_')[0]
-	file = open("./gene/" + strain + "_gene.xml", "w")
-	print >> file, template % strain
+	s = line.strip().split()[1].split('_')[0]
+	strain.append(s)
+
+for s in strain:
+	file = open("./gene/" + s + "_gene.xml", "w")
+	print >> file, template % s
+
+for s in strain:
+        file = open("./homolog/" + s + "_" + s + ".xml", "w")
+        print >> file, paralog_template % (s, s)
+
+for i, s1 in enumerate(strain):
+	for j, s2 in enumerate(strain[i + 1:]):
+		file = open("./homolog/" + s1 + "_" + s2 + ".xml", "w")
+		print >> file, hmlg_template % (s1, s2)
--- other/Mousemart/get.sh
+++ other/Mousemart/get.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 q=`<$1`
-wget -O result.txt "http://www.ensembl.org/biomart/martservice?query=$q"
+wget -O $2 "http://www.ensembl.org/biomart/martservice?query=$q"

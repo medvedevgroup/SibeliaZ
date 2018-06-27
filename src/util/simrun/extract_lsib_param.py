b39e4439560756bd699c679cd048a61ba388d501
ilyaminkin <ivminkin@gmail.com>
2018-06-27 15:36:10 -0400

    Update to the scripts

--- src/util/simrun/extract_lsib_param.py
+++ src/util/simrun/extract_lsib_param.py
@@ -4,9 +4,10 @@ from os import listdir
 from os.path import isfile, join, isdir
 import xml.etree.ElementTree as ET
 
-print "Best k,Best branch,Best flank,Recall,Precision,TwoPaco RT,TwoPaCo memory,L-Sibelia RT,L-Sibelia memory,spoa RT,spoa memory"
+print "Experiment,Best k,Best branch,Best flank,Recall,Precision,TwoPaCo RT,TwoPaCo memory,L-Sibelia RT,L-Sibelia memory,spoa RT,spoa memory"
 
-for experiment in ["1"]:
+for experiment_n in xrange(0, 9):
+	experiment = str(max(experiment_n * 3, 1))
 	mypath = join('./result/lsibelia/', experiment)
 	k_value = []
 	for f in listdir(mypath):
@@ -14,7 +15,7 @@ for experiment in ["1"]:
 		if isdir(p) and int(f) < 45:
 			k_value.append(int(f))
 	k_value.sort()
-	param_file = open("param" + experiment + ".txt", "w")
+	param_file = open("lsib_param" + experiment + ".txt", "w")
 	best_k,best_global_branch,best_global_flank,best_global_recall,best_global_precision=0,0,0,0,0
 	for now_k_value in k_value:
 		f = str(now_k_value)
@@ -36,17 +37,18 @@ for experiment in ["1"]:
 				root = tree.getroot()
 				now_result = []
 				for test in root.findall("homologyTests"):
-					now_result.append(float(test.find("aggregateResults").find("all").attrib["average"]))
+					val = float(test.find("aggregateResults").find("all").attrib["average"]) * 100
+					now_result.append(int(round(val)))
 				recall, precision = now_result
 				result[branch][flank] = (recall, precision)
 				rt_memory = []
-				for line in open(os.path.join(p2, "rt1.txt")):
+				for line in open(os.path.join(p2, "rt" + str(experiment) + ".txt")):
 					line = line.strip().split()
-					rt_memory.append(line[1])
+					rt_memory.append(int(round(float(line[1]))))
 					rt_memory.append(int(line[3]) / 1000)
-				if recall > best_recall:
+				if recall >= best_recall:
 					best_recall, best_branch, best_flank = recall, branch, flank
-				if recall > best_global_recall:
+				if recall >= best_global_recall:
 					best_k,best_global_branch,best_global_flank,best_global_recall,best_global_precision = now_k_value, branch, flank, recall, precision
 					best_rt_memory = rt_memory
 				if flank not in flanks:
@@ -63,6 +65,6 @@ for experiment in ["1"]:
 					xy.append(result[key][fl])
 				print >> param_file, str(key) + "," + ",".join(str(x) + "/" + str(y) for (x, y) in xy)
 			print >> param_file, ""
-	print ','.join(str(x) for x in [best_k,best_global_branch,best_global_flank,best_global_recall,best_global_precision] + best_rt_memory)
+	print ','.join(str(x) for x in [experiment, best_k,best_global_branch,best_global_flank,best_global_recall,best_global_precision] + best_rt_memory)
 
 
--- src/util/simrun/extract_sib_param.py
+++ src/util/simrun/extract_sib_param.py
@@ -4,44 +4,58 @@ from os import listdir
 from os.path import isfile, join, isdir
 import xml.etree.ElementTree as ET
 
-experiment = sys.argv[1]
-mypath = join('./result/sibelia/', experiment)
-k_value = []
-for f in listdir(mypath):
-        p = join(mypath, f)
-	if isdir(p) and int(f) < 45:
-		k_value.append(int(f))
 
-k_value.sort()
-for now_k_value in k_value:
-	f = str(now_k_value)
-	p = join(mypath, str(now_k_value))
-        if isdir(p):
-		print 'k =', f
-		result = dict()
-		best_recall = 0
-		best_branch = 0
-		for f2 in listdir(p):
-			p2 = join(p, f2)
-			branch = int(f2)
-			if branch not in result:
-				result[branch] = dict()
-			path = os.path.join(p2, "out.xml")
-			tree = ET.parse(path)
-			root = tree.getroot()
-			now_result = []
-			for test in root.findall("homologyTests"):
-				now_result.append(float(test.find("aggregateResults").find("all").attrib["average"]))
-			recall, precision = now_result
-			result[branch] = (recall, precision)
-			if recall > best_recall:
-				best_recall, best_branch = recall, branch
-		print "Best recall =", best_recall
-		print "Branch =", best_branch
-		print "branch,result"
-		keys = result.keys()
-		keys.sort()
-		for key in keys:
-			print str(key) + "," + str(result[key][0]) + "/" + str(result[key][1])
-		print ""
+print "Experiment,Best k,Best branch,Recall,Precision,Sibelia RT,Sibelia memory,spoa RT,spoa memory"
+
+for experiment_n in xrange(0, 9):
+        experiment = str(max(experiment_n * 3, 1))
+	mypath = join('./result/sibelia/', experiment)
+	k_value = []
+	for f in listdir(mypath):
+        	p = join(mypath, f)
+		if isdir(p) and int(f) < 45:
+			k_value.append(int(f))
+	k_value.sort()
+	param_file = open("sib_param" + experiment + ".txt", "w")
+	best_k,best_global_branch,best_global_recall,best_global_precision=(0,0,0,0)
+	for now_k_value in k_value:
+		f = str(now_k_value)
+		p = join(mypath, str(now_k_value))
+        	if isdir(p):
+			print >> param_file, 'k =', f
+			result = dict()
+			best_recall = 0
+			best_branch = 0
+			for f2 in listdir(p):
+				p2 = join(p, f2)
+				branch = int(f2)
+				path = os.path.join(p2, "out.xml")
+				tree = ET.parse(path)
+				root = tree.getroot()
+				now_result = []
+				for test in root.findall("homologyTests"):
+                                        val = float(test.find("aggregateResults").find("all").attrib["average"]) * 100
+                                        now_result.append(int(round(val)))
+				recall, precision = now_result
+				result[branch] = (recall, precision)
+				rt_memory = []
+				for line in open(os.path.join(p2,  "rt" + str(experiment) + ".txt")):
+					line = line.strip().split()
+					rt_memory.append(int(round(float(line[1]))))
+					rt_memory.append(int(line[3]) / 1000)
+				if recall >= best_recall:
+					best_recall, best_branch = recall, branch
+				if recall >= best_global_recall:
+					best_k,best_global_branch,best_global_recall,best_global_precision = now_k_value, branch,  recall, precision
+					best_rt_memory = rt_memory
+			print >> param_file, "Best recall =", best_recall
+			print >> param_file, "Branch = ", best_branch
+			print >> param_file, "Branch,Recall/Precision"
+			keys = result.keys()
+			keys.sort()
+			for key in keys:
+				print >> param_file, str(key) + "," + str(result[key][0]) + "/" + str(result[key][1])
+			print >> param_file, ""
+	print ','.join(str(x) for x in [experiment,best_k,best_global_branch,best_global_recall,best_global_precision] + best_rt_memory)
+
 

cd17140eba086b1869e9a84393889e10623b6d9c
ilyaminkin <ivminkin@gmail.com>
2019-01-15 14:55:33 -0500

    Fixed

---
+++ SibeliaZ-LCB/distancekeeper.h
@@ -0,0 +1,43 @@
+#ifndef _DISTANCE_KEEPER_H_
+#define _DISTANCE_KEEPER_H_
+
+#include "junctionstorage.h"
+
+namespace Sibelia
+{
+	class DistanceKeeper
+	{
+	public:
+		DistanceKeeper(int64_t vertices) : vertices_(vertices), NOT_SET(INT_MAX)
+		{
+			distance_.assign(vertices_ * 2, NOT_SET);
+		}
+
+		bool IsSet(int64_t v) const
+		{
+			return distance_[v + vertices_] != NOT_SET;
+		}
+
+		void Set(int64_t v, int distance)
+		{
+			distance_[v + vertices_] = distance;
+		}
+
+		int Get(int64_t v) const
+		{
+			return distance_[v + vertices_];
+		}
+
+		void Unset(int64_t v)
+		{
+			distance_[v + vertices_] = NOT_SET;
+		}
+
+	private:
+		int64_t vertices_;
+		const int NOT_SET;
+		std::vector<int> distance_;
+	};
+}
+
+#endif
---
+++ SibeliaZ-LCB/outputgenerator.h
@@ -0,0 +1,9 @@
+#ifndef _OUTPUT_GENERATOR_H_
+#define _OUTPUT_GENERATOR_H_
+
+namespace Sibelia
+{
+
+}
+
+#endif

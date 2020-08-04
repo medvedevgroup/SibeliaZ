6cacf602920940624daad9f43bdfa5c4fd4adff1
ilyaminkin <ivminkin@gmail.com>
2020-08-04 17:50:20 +0700

    More accurate overlaps

--- SibeliaZ-LCB/blocksfinder.h
+++ SibeliaZ-LCB/blocksfinder.h
@@ -319,11 +319,19 @@ namespace Sibelia
 
 					if (jt.Front().IsPositiveStrand())
 					{
-						finder.blocksInstance_.push_back(BlockInstance(+currentBlock, jt.Front().GetChrId(), jt.Front().GetPosition(), jt.Back().GetPosition() + finder.k_));
+						auto before = jt.Front() - 1;
+						auto start = jt.Front().GetPosition() + ((before.Valid() && before.IsUsed()) ?  finder.k_ : 0);
+						auto end = jt.Back().GetPosition() + (jt.Back().IsUsed() ? 0 : finder.k_);
+						finder.blocksInstance_.push_back(BlockInstance(+currentBlock, jt.Front().GetChrId(), start, end));
 					}
 					else
 					{
-						finder.blocksInstance_.push_back(BlockInstance(-currentBlock, jt.Front().GetChrId(), jt.Back().GetPosition() - finder.k_, jt.Front().GetPosition()));
+						auto before = jt.Front() - 1;
+						auto start = jt.Back().GetPosition() - (jt.Back().IsUsed() ? 0 : finder.k_);
+						auto end = jt.Front().GetPosition() + (before.Valid() && before.IsUsed() ? 0 : finder.k_);
+						finder.blocksInstance_.push_back(BlockInstance(-currentBlock, jt.Front().GetChrId(), start, end));
+
+//						finder.blocksInstance_.push_back(BlockInstance(-currentBlock, jt.Front().GetChrId(), jt.Back().GetPosition() - finder.k_, jt.Front().GetPosition()));
 					}
 
 					//finder.log_ << jt.Front().GetChrId() << ' ' << jt.Front().GetPosition() << ' ' << jt.Back().GetPosition() << std::endl;
